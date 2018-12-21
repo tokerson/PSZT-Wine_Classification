f163660d535d333b3c8282322659de0b58b78d2d
tokerson <kuba.tokarzewski@gmail.com>
2018-12-21 19:44:57 +0100

    deleted network2 class, updated doc

--- src/Network.py
+++ src/Network.py
@@ -1,53 +1,80 @@
 import numpy as np
 
-#class describing how our network looks like
-#Fields:    inputs - array of 12 Integers from 0 to 10
-#           hidden_neurons - size of hidden_layer ( number of hidden neurons)
-#           hidden_layer - array containing objects with two fields ( 'weights' is an array of weights of the edges coming from the input,
-#                                                                     'bias' is a value added in neuron)
-#           hidden_layer[2]['weights'][0] is a weight of the edge starting in the 1st input neuron and ending in the 3rd hidden neuron
-#            
 class Network:
 
-    #user specyfies number of input nodes and how big should the hidden layer be
-    def __init__(self, hidden_neurons = 20):
-        self.number_of_inputs = 12
-        self.hidden_neurons = 4#int(np.sqrt(12))
-        self.inputs = [ 0, 1 , 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11 ]
-        #self.inputs = np.random.randint(low = 0 , high = 11, size = self.number_of_inputs) # array of random integers from 0 to 10 
-        self.hidden_layer = [ { 'weights': np.random.uniform(low = -1.0 / np.sqrt(self.number_of_inputs), 
-                                                             high = 1.0 / np.sqrt(self.number_of_inputs), 
-                                                             size = self.number_of_inputs),  
-                                'bias': 0.0} for i in range(self.hidden_neurons) ]
-        self.output_neuron =  { 'weights': np.random.uniform(low = -1.0 / np.sqrt(self.hidden_neurons), 
-                                                             high = 1.0 / np.sqrt(self.hidden_neurons)  , 
-                                                             size = self.hidden_neurons),
-                                 'bias' : 0.0 }
+    def __init__(self, inputSize = 12, hiddenSize = 4):
+        self.outputSize = 1
+        self.learningRate = 0.05
+        self.W1 = np.random.randn(inputSize, hiddenSize) / np.sqrt(inputSize)
+        self.W2 = np.random.randn(hiddenSize, self.outputSize) / np.sqrt(hiddenSize)
+        self.B1 = np.zeros((1,hiddenSize))
+        self.B2 = np.zeros((1,self.outputSize))
 
-        
     def sigmoid(self, x):
-        return 1/(1 + np.exp(-x))
+        return 1 / ( 1 + np.exp(-x))
+    
+    def sigmoidPrime(self, x):
+        return x * ( 1 - x)
 
-    def feed_forward(self):
-        output = 0.0
-        result = [0] * self.hidden_neurons  #creating an array of size equals to hidden neurons number and filled with zeros.
-        #foreach hidden neuron we are calculating its output based on the weights of the edges
-        for j in range(0, self.hidden_neurons):
-
-            for i in range(0, self.number_of_inputs):
-                result[j] += self.inputs[i] * self.hidden_layer[j]['weights'][i] 
-            
-            result[j] += self.hidden_layer[j]['bias']
-            result[j] = self.sigmoid(result[j])
+    def feed_forward(self, input):
         
-        for i in range(0, self.hidden_neurons):
-            output += result[i] * self.output_neuron['weights'][i]
+        self.Z1 = np.dot(input, self.W1) + self.B1
+        self.A1 = self.sigmoid(self.Z1)
+        self.Z2 = np.dot(self.A1, self.W2) + self.B2
+        output = self.sigmoid(self.Z2)
 
-        output += self.output_neuron['bias']
-        return self.sigmoid(output)
+        return output
+
+    def backward(self,X, expected, calculated ):
+        self.loss = (expected - calculated)      # (n x 1)
+
+        self.dZ2 = self.loss * self.sigmoidPrime(calculated)
+        self.dW2 = np.dot(self.A1.T, self.dZ2) / len(X)                    # (n x h)^T(n x 1) = (h x n)(n x 1)= (h x 1)
+        self.dB2 = np.sum(self.loss.T, axis = 1, keepdims = True) / len(X)    
+        
+        self.dZ1 = np.dot(self.dZ2, self.W2.T)*self.sigmoidPrime(self.A1)   # (n x h) = (n x 1)(h x 1)^ T = (n x 1)(1 x h) 
+        self.dW1 = np.dot(X.T, self.dZ1) / len(X)                              # ( m x h) = ( n x m)^T(n x h) = (m x n)(n x h)
+        self.dB1 = np.sum(self.dZ1.T, axis = 1, keepdims = True) / len(X)     # ( 1 x h)
+        
+        self.W1 += self.learningRate*self.dW1
+        self.W2 += self.learningRate*self.dW2
+  
+        self.B1 += self.learningRate*self.dB1.T
+        self.B2 += self.learningRate*self.dB2
+
+
+    def train (self, X , Y):
+        output = self.feed_forward(X)
+        print("Output")
+        print(output)
+        print("Expected output")
+        print(Y)
+        self.backward(X,Y,output)
+    
 
 network = Network()
-    
-print("Output", network.feed_forward())
-    
+X = np.array((
+    [ 0, 1 , 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11 ],
+    [ 1, 3, 4 ,5 ,6 ,7, 1, 2, 4, 5, 2 ,1],
+    [ 0, 1 , 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11 ],
+    [ 1 ,2 , 3 ,1 ,2 ,4 ,5 ,0.5 ,6 ,2 , 4 ,1],
+    [ 1 ,5 , 1 ,2 ,2 ,4 ,5 ,1.5 ,6 ,2 , 4 ,1]
+), dtype=float)
+Y = np.array((
+      [0.5], 
+      [0.35],
+      [0.5],
+      [0.8],
+      [0.63]
+), dtype=float)  
+for i in range(5000):
+    network.train(X,Y)
+
+print("B1")
+print(network.B1)
+print("B2")
+print(network.B2)
+
+print("check")
+print(network.feed_forward([[ 0, 1 , 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11 ]]))
 
