be0360c5a75eb8329a8ee09f6af9f513548ae2d5
tokerson <kuba.tokarzewski@gmail.com>
2019-01-14 19:56:57 +0100

    drawing diagrams in patplotlib

--- src/training.py
+++ src/training.py
@@ -1,6 +1,12 @@
 from CsvReader import *
 from Network import Network, save_network_to_file, load_network_from_file, get_rating
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
 wines = get_normalized_data('../data/winequality-red.csv')
 poor_wines = get_poor_wines(wines)
 good_wines = get_good_wines(wines)
@@ -19,7 +25,7 @@ average_wines = get_average_wines(wines)
 # seperate_inputs_and_outputs(average_wines, average_wines_inputs, average_wines_outputs)
 
 
-elements = 1000
+elements = 500
 
 training_input_set = []
 training_output_set = []
@@ -41,6 +47,7 @@ testing_input_set = []
 testing_output_set = []
 testing_set = []
 
+
 for data in get_testing_data(int(elements / 21), len(poor_wines), poor_wines):
     # for i in range(0, 21):
         testing_set.append(copy.deepcopy(data))
@@ -56,34 +63,41 @@ seperate_inputs_and_outputs(testing_set, testing_input_set, testing_output_set)
 network = Network()
 network.learningRate = 0.2
 
-# print(training_input_set)
-# print(training_output_set)
+n_epoch = 100
+times = []
+ratios = []
+epochs = []
+for i in range(10 , n_epoch, 10):
+        start = time.time()
+        epochs.append(i)
+        for j in range(0 , i):
+                loss_sum = 0
+                for k in range(0, len(training_input_set)):
+                        result = network.feed_forward(training_input_set[k])
+                        network.backward_propagation(training_output_set[k][0], result, training_input_set[k])
+                        loss_sum += abs(result - training_output_set[k][0])
 
-n_epoch = 200
+        wrong = 0
+        correct = 0
+        # print("\n===TESTING===\n")
+        for l in range(0, len(testing_input_set)):
+                result = network.feed_forward(testing_input_set[l]) \
+                        # if round(result, 1) == testing_outputs[i][0]:
+                if get_rating(result) == get_rating(testing_output_set[l][0]):
+                        # print("ROW %d - CORRECT" % i)
+                        # print(result, testing_output_set[i][0])
+                        correct += 1
+                else:
+                        # print("ROW %d - WRONG!!! \n %f %f" % (i, result, testing_output_set[i][0]))
+                        # print(result, testing_output_set[i][0])
+                        wrong += 1
 
-for i in range(0, n_epoch):
-    loss_sum = 0
-    for j in range(0, len(training_input_set)):
-        result = network.feed_forward(training_input_set[j])
-        network.backward_propagation(training_output_set[j][0], result, training_input_set[j])
-        # print("Epoch= %d, data_row=%f, error=%f, expected=%f" % (i, j, result, training_outputs[j][0]))
-        loss_sum += abs(result - training_output_set[j][0])
+        end = time.time()
+        times.append(end - start)
+        ratios.append(correct / len(testing_input_set) * 100)
+        #print("CORRECT: %d WRONG: %d  ratio = %f" % (correct, wrong, correct / len(testing_input_set) * 100))
 
-    #  print("Epoch %d, loss sum = %f" % (i, loss_sum))
-
-wrong = 0
-correct = 0
-print("\n===TESTING===\n")
-for i in range(0, len(testing_input_set)):
-    result = network.feed_forward(testing_input_set[i]) \
-        # if round(result, 1) == testing_outputs[i][0]:
-    if get_rating(result) == get_rating(testing_output_set[i][0]):
-        print("ROW %d - CORRECT" % i)
-        print(result, testing_output_set[i][0])
-        correct += 1
-    else:
-        print("ROW %d - WRONG!!! \n %f %f" % (i, result, testing_output_set[i][0]))
-        print(result, testing_output_set[i][0])
-        wrong += 1
-
-print("CORRECT: %d WRONG: %d  ratio = %f" % (correct, wrong, correct / len(testing_input_set) * 100))
+plt.plot( epochs, ratios , linestyle="-", marker='o')
+plt.xlabel("epochs")
+plt.ylabel("ratio")
+plt.savefig('../diagrams/testplot.png')
